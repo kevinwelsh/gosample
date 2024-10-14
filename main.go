38c51c7650bc9d3a81258754e4e91869bedfe8f0
Kevin Welsh <kevinwelsh99@gmail.com>
2024-10-14 15:55:18 +0100

    Add untested functions

--- main.go
+++ main.go
@@ -10,3 +10,18 @@ func main() {
 func HelloWorld() string {
 	return "hello world"
 }
+
+// GoodMorningWorld is a function that returns a string containing "good morning world".
+func GoodMonrningWorld() string {
+	return "good morning world"
+}
+
+// HelloWorld is a function that returns a string containing "hello world".
+func GoodMonrningWorld() string {
+	return "good morning world"
+}
+
+// GoodEveningWorld is a function that returns a string containing "good evening world".
+func GoodMonrningWorld() string {
+	return "good evening world"
+}
